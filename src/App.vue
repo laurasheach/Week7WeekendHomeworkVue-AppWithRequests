6442583991516a3a599c730f149477087e242981
Laura Sheach <laura_bubble_git@yahoo.com>
2019-06-21 12:25:25 +0100

    Linked to Guardian API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-
+    <h1>Guardian News Search</h1>
+    <article-list :news="news"></article-list>
   </div>
 </template>
 
 <script>
-
+import ArticleList from './components/ArticleList.vue'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      news: {}
+    }
+  },
+
   components: {
-    
+    "article-list": ArticleList
+  },
+
+  mounted(){
+    fetch("https://content.guardianapis.com/search?q=news&format=json&api-key=test")
+    .then(result => result.json())
+    .then(news => this.news = news.response)
   }
+
 }
 </script>
 
---
+++ src/components/ArticleList.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div id="">
+    <h2>{{news.results}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'news-list',
+  props: ['news']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
