a13e2f27222b2d5eb1e00ec0e4baef031aa0ab87
Laura Sheach <laura_bubble_git@yahoo.com>
2019-06-21 18:51:44 +0100

    Link to web page now working

--- src/components/ArticleListDetail.vue
+++ src/components/ArticleListDetail.vue
@@ -2,14 +2,23 @@
 
   <div v-if="article" id="articleDetail">
     <p><span>Guardian Section Name</span>: {{article.sectionName}}</p>
+    <p><span>Publication Date</span>: {{article.webPublicationDate}}</p>
+    <a :href="article.webUrl">Visit Article</a>
   </div>
 
 </template>
 
 <script>
+import {eventBus} from '@/main.js'
+
 export default {
   name: 'article-detail',
-  props: ['article']
+  props: ['article'],
+  methods: {
+    handleClick(){
+      eventBus.$emit('article-selected', this.article)
+    }
+  }
 }
 </script>
 
