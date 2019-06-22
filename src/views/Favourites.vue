dc5cc8b760b108daab43f7385ff0076ccd549d4f
Laura Sheach <laura_bubble_git@yahoo.com>
2019-06-22 12:00:40 +0100

    Article detail now available in favourites

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -1,18 +1,21 @@
 <template lang="html">
   <div id="">
     <h1>Favourites</h1>
-    <article-list :articles="favourites" :favourites="favourites"></article-list>
+      <article-list :articles="favourites" :favourites="favourites"></article-list>
+      <article-list-detail :article="article"></article-list-detail>
   </div>
 </template>
 
 <script>
 import ArticleList from '@/components/ArticleList.vue'
+import ArticleListDetail from '@/components/ArticleListDetail.vue'
 
 export default {
   name: 'favourites',
-  props: ['favourites'],
+  props: ['favourites', 'article'],
   components: {
-    'article-list': ArticleList
+    'article-list': ArticleList,
+    "article-list-detail": ArticleListDetail
   }
 }
 </script>
